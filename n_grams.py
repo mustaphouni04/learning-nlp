6fa3676e007143087c6f5e3fe96106df1197988a
mustaphouni04 <elaichounimustapha@gmail.com>
2025-02-27 18:45:00 +0100

    ngrams

--- n_grams.py
+++ n_grams.py
@@ -3,6 +3,12 @@ from nltk import NLTKWordTokenizer, Text
 from nltk.util import ngrams
 import re
 from nltk.probability import FreqDist
+import random
+from nltk.util import pad_sequence
+from nltk.lm.preprocessing import pad_both_ends, flatten, padded_everygram_pipeline
+from nltk.lm import MLE
+from nltk.util import everygrams
+
 
 sent = """
 To Sherlock Holmes she is always 'The Woman'. 
@@ -11,7 +17,10 @@ I have seldom heard him mention her under any other name
 
 tokens = nltk.word_tokenize(sent)
 
+print("2-gram")
 print(list(ngrams(tokens, 2, pad_right=True)))
+print("\n")
+print("3-gram")
 print(list(ngrams(tokens, 3, pad_right=True)))
 
 two_grams = ngrams(tokens, 2)
@@ -22,11 +31,70 @@ two_grams = ngrams(nltk.word_tokenize(clean), 2)
 fdist = FreqDist()
 
 for gram in two_grams:
-    fdist[(gram[0].lower(), gram[1].lower())] += 1
+    fdist[(gram[0], gram[1].lower())] += 1
 
-print(fdist.items())
+print("frequencies")
+print({k:v for k,v in sorted(fdist.items(), key= lambda x:x[1], reverse=True)})
 
-with open("Adventures_Holmes.txt", "r") as f:
+with open("moby_dick.txt", "r") as f:
     txt = f.read()
+txt = re.sub(r'[^a-zA-Z0-9 ]', '', txt)
+tokens = nltk.word_tokenize(txt)
+grams = [2,3,4]
+print("\n")
+for g in grams:
+    gramos = ngrams(tokens, g)
+
+    fdist = FreqDist()
+
+    for gram in gramos:
+        fdist[gram] += 1
+ 
+    print(f"for {g}-gram: ", list({k:v for k,v in sorted(fdist.items(), key= lambda x:x[1], reverse=True)}.items())[:9])
+
+    print("\n\n")
+
+paragraphs = []
+paragraph = []
+with open("Adventures_Holmes.txt", "r") as f:
+    for line in f.readlines():
+        if line == "\n":
+            paragraphs.append(paragraph)
+            paragraph = []
+        else:
+            paragraph.append([line]) 
+
+test_paragraph = paragraphs[random.randint(0,len(paragraphs))]
+test_paragraph2 = [nltk.word_tokenize(line[0]) for line in test_paragraph]
+print("GROUND TRUTH\n")
+test_paragraph2 = [word for line in test_paragraph2 for word in line]
+print(" ".join(test_paragraph2))
+train_paragraphs = [nltk.word_tokenize(line[0]) for paragraph in paragraphs for line in paragraph if paragraph != []]
+lm = MLE(2)
+
+train, vocab = padded_everygram_pipeline(2, train_paragraphs)
+lm.fit(train, vocab)
+
+
+print("SEED")
+print(" ".join(test_paragraph2[:5]))
+print("GENERATION")
+count = 0
+r = 0
+temp_ls = []
+for idx,word in enumerate(test_paragraph2):
+    count += 1
+    temp_ls.append(word)
+    word_gen = lm.generate(1,text_seed=temp_ls)
+    if idx == len(test_paragraph2) - 1:
+        break
+    else:
+        gt = test_paragraph2[idx+1]
+        if word_gen == gt:
+            r += 1
+print("WAR")
+print(r/len(test_paragraph2))
+print(" ".join(lm.generate(len(test_paragraph2), text_seed=test_paragraph2[:5])))
+
 
 
---
+++ word_normalization.py
@@ -0,0 +1,89 @@
+import nltk
+from nltk.stem.porter import *
+import io
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+with open("Firefox.txt", "r", encoding='latin-1') as f:
+    txt = f.read()
+
+tokens = nltk.word_tokenize(txt)
+
+vocabulary = set(tokens)
+print(len(vocabulary))
+stemmer = PorterStemmer()
+
+singles1 = [stemmer.stem(token) for token in tokens]
+
+print(singles1[:5])
+vocabulary1 = set(singles1)
+print(len(vocabulary1))
+
+stemmer1 = SnowballStemmer("english")
+
+singles2 = [stemmer1.stem(token) for token in tokens]
+vocabulary2 = set(singles2)
+print(singles2[:5])
+
+print(len(vocabulary2))
+
+stopwords = stopwords.words('english')
+singles1_s = [word for word in singles1 if word not in stopwords]
+singles2_s = [word for word in singles2 if word not in stopwords]
+
+print(len(set(singles1_s)))
+print(len(set(singles2_s)))
+
+fdist = FreqDist()
+
+for word in singles1:
+    fdist[word.lower()] += 1
+
+fdist = {k:v for k,v in sorted(fdist.items(), key= lambda x:x[1], reverse=True)}
+
+top_30 = list(fdist.items())[:29]
+
+fig, ax = plt.subplots()
+words = [tup[0] for tup in top_30]
+counts = [tup[1] for tup in top_30]
+ax.bar(words,counts)
+ax.set_ylabel('frequency')
+ax.set_title('top 30 most frequent words')
+plt.xticks(rotation=45, ha="right")
+plt.show()
+
+fdist = FreqDist()
+
+for word in singles1_s:
+    fdist[word.lower()] += 1
+
+fdist = {k:v for k,v in sorted(fdist.items(), key= lambda x:x[1], reverse=True)}
+
+top_30 = list(fdist.items())[:29]
+
+fig, ax = plt.subplots()
+words = [tup[0] for tup in top_30]
+counts = [tup[1] for tup in top_30]
+ax.bar(words,counts)
+ax.set_ylabel('frequency')
+ax.set_title('top 30 most frequent words')
+plt.xticks(rotation=45, ha="right")
+plt.show()
+
+
+cst_stp = "doe"
+stopwords.append(cst_stp)
+
+singles1_s = [word for word in singles1_s if word not in stopwords]
+print(len(set(singles1_s)))
+
+wordcloud = WordCloud(stopwords=stopwords, background_color='black', max_words=300).generate_from_frequencies(fdist)
+
+plt.figure(figsize=(15,10))
+plt.clf()
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
