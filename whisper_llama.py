f134ff4a0b9f617960344ac774c75083e190e642
mustaphouni04 <elaichounimustapha@gmail.com>
2025-05-17 00:33:13 +0200

    refactored a lil bit

--- whisper_llama.py
+++ whisper_llama.py
@@ -3,11 +3,42 @@ from datasets import load_dataset
 import aiohttp
 import numpy as np 
 import soundfile as sf
+from dataclasses import dataclass
+import typing
 
-# load model and processor
-processor = WhisperProcessor.from_pretrained("openai/whisper-large-v3")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large-v3")
-model.config.forced_decoder_ids = None
+@dataclass
+class AudioFile:
+    path: str 
+    array: np.ndarray
+    sampling_rate: int
+
+def save_audio_file(audio_sample: AudioFile):
+    audio_file = AudioFile(path=audio_sample["path"], 
+                           array=audio_sample["array"],
+                           sampling_rate=audio_sample["sampling_rate"])
+    if np.issubdtype(audio_file.array.dtype, np.floating):
+        audio_array = (audio_file.array * 32767).astype(np.int16)  
+
+    sf.write("output.wav", audio_array, sampling_rate, subtype='PCM_16')
+    print("Saved as output.wav")
+    return 0
+     
+class WhisperCaller:
+    def __init__(self, model_name: str):
+        self.processor = WhisperProcessor.from_pretrained(model_name)
+        self.model = WhisperForConditionalGeneration.from_pretrained(model_name)
+        self.model.config.forced_decoder_ids = None
+
+    def generate_text_from_audio(self, sample: AudioFile):
+        audio_file = AudioFile(path=sample["path"], 
+                               array=sample["array"],
+                               sampling_rate=sample["sampling_rate"])
+
+        input_features = self.processor(audio_file.array, sampling_rate=audio_file.sampling_rate, return_tensors="pt").input_features 
+        predicted_ids = self.model.generate(input_features)
+        transcription = self.processor.batch_decode(predicted_ids, skip_special_tokens=True)
+
+        return transcription
 
 # load dataset and read audio files
 ds = load_dataset("librispeech_asr", "clean", split="test",
@@ -15,23 +46,12 @@ ds = load_dataset("librispeech_asr", "clean", split="test",
 sample = ds[0]["audio"]
 audio_array = sample['array']
 sampling_rate = sample['sampling_rate']
-if np.issubdtype(audio_array.dtype, np.floating):
-    audio_array = (audio_array * 32767).astype(np.int16)  
 
-sf.write("output.wav", audio_array, sampling_rate, subtype='PCM_16')
-print("Saved as output.wav")
+save_audio_file(sample)
 
-print(sample)
-input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
+whisper = WhisperCaller("openai/whisper-large-v3")
+print(whisper.generate_text_from_audio(sample))
 
-# generate token ids
-predicted_ids = model.generate(input_features)
 
-# decode token ids to text
-transcription = processor.batch_decode(predicted_ids, skip_special_tokens=False)
-#['<|startoftranscript|><|en|><|transcribe|><|notimestamps|> Mr. Quilter is the apostle of the middle classes and we are glad to welcome his gospel.<|endoftext|>']
 
-transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
-#[' Mr. Quilter is the apostle of the middle classes and we are glad to welcome his gospel.']
-print(transcription)
 
